4ea7d62334d35335eaa6d97d2779164e1d4ee6cf
Anushthan Saxena <anushthans@gmail.com>
2024-02-22 17:46:29 +0530

    Landing page css modifications; footer changed; removed useless icons

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,14 +1,9 @@
 import logo from './images/WizlanceLogo.png';
 import menu from './menu.svg';
 import close from './close.svg';
-import s1_icon from './s1.png';
-import s2_icon from './s2.png';
-import s3_icon from './s3.png';
-import s4_icon from './s4.png';
 
 export {
     logo,
     menu,
-    close,
-    s1_icon, s2_icon, s3_icon, s4_icon
+    close
 }
