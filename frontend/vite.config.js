dfa1995b1d847fc1f844a6069255f09fbca51dfc
Anushthan Saxena <anushthans@gmail.com>
2024-04-29 18:40:26 +0530

    vercel config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,16 @@ import react from '@vitejs/plugin-react'
 
 const config = defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        // target: `http://localhost:3000`,
-        target: 'https://wizlance-fdfed.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      }
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       // target: `http://localhost:3000`,
+  //       target: 'https://wizlance-fdfed.onrender.com',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     }
+  //   },
+  // },
 })
 
 export default config;
