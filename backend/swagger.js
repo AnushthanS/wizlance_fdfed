e61eb737a33bec88f8427c65242f02074bb1de69
Anushthan Saxena <anushthans@gmail.com>
2024-05-01 10:37:44 +0530

    added missing swagger config

---
+++ backend/swagger.js
@@ -0,0 +1,27 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+function swaggerDocs(app, port) {
+    const options = {
+        definition: {
+            openapi: '3.0.0',
+            info: {
+                title: 'FitWell API',
+                version: '1.0.0',
+                description: 'API required for a fitness application',
+            },
+            servers: [
+                {
+                    url: `http://localhost:${port}`,
+                },
+            ],
+        },
+        apis: ['./routes/*.js'],
+    };
+    
+    const swaggerSpec = swaggerJsdoc(options);
+    
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
+
+module.exports = swaggerDocs;
